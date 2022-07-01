f3552f22c917110a61ff4326ce7388cc974916b3
unknown <akshay.khanna0810@gmail.com>
2022-07-01 19:09:33 +0530

    Splash screen completed

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, ScrollView, ImageBackground, Dimensions, TouchableOpacity } from 'react-native';
-import LinearGradient from 'react-native-linear-gradient';
+import { StyleSheet, Text, View, ScrollView, ImageBackground, Dimensions, TouchableOpacity, Image } from 'react-native';
+import { LinearGradient } from 'expo-linear-gradient';
 
 export default function App() {
   return (
@@ -13,24 +13,46 @@ export default function App() {
         blurRadius={1}
         resizeMode='cover'
       >
+        <LinearGradient
+          colors={['rgba(0,0,0,0.00)', 'rgba(0,0,0,1)']}
+          style={styles.gradientBackground}
+        />
         
         <View style={styles.upperView}>
+          <ImageBackground
+              source={require('./assets/spotifyLogo.png')}
+              style={styles.spotifyIcon}
+              resizeMode='cover'
+            />
           <Text style={styles.titleText1}>Enjoy Millions of Songs</Text>
-          <Text style={styles.titleText2}>Spring Play</Text>
+          <Text style={styles.titleText2}>On Spotify.</Text>
         </View>
       </ImageBackground>
 
       <View style={styles.lowerView}>
+        {/* signup */}
         <TouchableOpacity style={styles.btnSignUp}>
           <Text style={styles.textSignUp}> Sign up for free </Text>
         </TouchableOpacity>
-      </View>
-      
-      <View style={styles.lowerView}>
-        <TouchableOpacity style={styles.btnSignUp}>
+
+        {/* google sign in */}
+        <TouchableOpacity style={styles.btnLogin}>
+          <ImageBackground
+            source={require('./assets/googleLogo.png')}
+            style={styles.googleIcon}
+            resizeMode='cover'
+          />
+          <Text style={styles.textSignUp}> Continue with google </Text>
+        </TouchableOpacity>
+
+        {/* login */}
+        <TouchableOpacity style={styles.btnLogin}>
           <Text style={styles.textSignUp}> Log in </Text>
         </TouchableOpacity>
+
+
       </View>
+
     </ScrollView>
   );
 }
@@ -41,12 +63,26 @@ const styles = StyleSheet.create({
     backgroundColor: '#000'
   },
   backImage: {
-    height: Dimensions.get('window').height / 1.75,
+    height: Dimensions.get('window').height / 1.5,
+  },
+  gradientBackground: {
+    position: 'absolute',
+    left: 0,
+    right: 0,
+    top: 0,
+    height: '100%',
+    width: '100%'
   },
   upperView: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center'
+  },
+  spotifyIcon: {
+    position: 'absolute',
+    bottom: 100,
+    height: 50,
+    width: 50
   },
   titleText1: {
     position: 'absolute',
@@ -57,7 +93,7 @@ const styles = StyleSheet.create({
   },
   titleText2: {
     position: 'absolute',
-    color: '#FF7518',
+    color: '#fff',
     fontSize: 28,
     fontWeight: '600',
     bottom: 10
@@ -72,12 +108,32 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     padding: 10,
     width: Dimensions.get('window').width / 1.25,
-    backgroundColor: '#FF7518',
     borderRadius: 25,
-    marginTop: 30
+    marginTop: 30,
+    backgroundColor: '#21c45a',
+  },
+  btnLogin: {
+    flex: 1,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-around',
+    padding: 10,
+    width: Dimensions.get('window').width / 1.25,
+    borderRadius: 25,
+    marginTop: 10,
+    borderWidth: 1,
+    backgroundColor: '#000',
+    borderColor: '#fff'
   },
   textSignUp: {
     fontSize: 16,
+    fontWeight: '500',
     color: '#fff'
+  },
+  googleIcon: {
+    position: 'absolute',
+    left: 16,
+    height: 16,
+    width: 16
   }
 });
